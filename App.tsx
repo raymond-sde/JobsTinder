b0a90688b4b42d8ade2c55333d94062439cb85a1
Chaynor Hsiao <chaynsaw@gmail.com>
2021-04-08 14:09:16 -0700

    Remove commented code

--- App.tsx
+++ App.tsx
@@ -1,28 +1,3 @@
-// import { StatusBar } from "expo-status-bar";
-// import React from "react";
-// import Button from "./components/TestPage";
-// import { StyleSheet, Text, View } from "react-native";
-// import TestPage from "./components/TestPage";
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>asdfff</Text>
-//       <TestPage />
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
-
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { ParentContainer } from "./components/ParentContainer";
 
