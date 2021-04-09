ba5d3ebf034b4d1ad46cb9e5ebe67c0967488aad
raymond-sde <raymond.aranda.sde@gmail.com>
2021-04-09 08:12:38 -0700

    Job card UI, added job mock data

---
+++ components/JobCard/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 0.45,
+    borderRadius: 8,
+    shadowRadius: 25,
+    shadowColor: "#000",
+    shadowOpacity: 0.08,
+    shadowOffset: { width: 0, height: 0 },
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    padding: 30,
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+});
