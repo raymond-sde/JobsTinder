1505fe4466191f883bc986583498b42efe555e66
Wenhao Piao <wenhaopiao520@gmail.com>
2021-04-09 08:34:04 -0700

    Build SearchForJobs component and search functionality

--- components/SearchForJobs.tsx
+++ components/SearchForJobs.tsx
@@ -1,10 +1,69 @@
-import * as React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView, TextInput, Button, StyleSheet } from "react-native";
+import axios from "axios";
 
 export const SearchForJobs = () => {
+  const [jobs, onChangeJobs] = useState("");
+  const [location, onChangeLocation] = useState("");
+  const [data, setData] = useState([]);
+
+  const handleSearch = () => {
+    let formattedJobs = "";
+    for (const letter of jobs) {
+      if (letter === " ") {
+        formattedJobs += "+";
+      } else {
+        formattedJobs += letter;
+      }
+    }
+
+    let formattedLocation = "";
+    for (const letter of location) {
+      if (letter === " ") {
+        formattedLocation += "+";
+      } else {
+        formattedLocation += letter;
+      }
+    }
+    const url = `https://jobs.github.com/positions.json?description=${formattedJobs}&location=${formattedLocation}`;
+    axios.get(url)
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => console.log(error));
+  }
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Search for Jobs!</Text>
-    </View>
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeJobs}
+        value={jobs}
+        placeholder="Search Jobs"
+      />
+      <TextInput />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeLocation}
+        value={location}
+        placeholder="Location"
+      />
+      <TextInput />
+      <Button
+        onPress={handleSearch}
+        title="Search"
+        color="#841584"
+      />
+      {/* placeholder, replace it with Card component */}
+      {data.map((job, i) => <TextInput key={i} value={job["title"]} />)}
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1
+  }
+});
