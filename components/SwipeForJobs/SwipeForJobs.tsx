5ff3d61e8c693250a7640de1cfe4ea405d606744
Chaynor Hsiao <chaynsaw@gmail.com>
2021-04-10 16:07:48 -0700

    Add saveOrRejectCurrentJob to onSwipedLeft and onSwipedRight

--- components/SwipeForJobs/SwipeForJobs.tsx
+++ components/SwipeForJobs/SwipeForJobs.tsx
@@ -12,9 +12,22 @@ type SwipeForJobsProps = {
 
 export const SwipeForJobs = ({ jobs }: SwipeForJobsProps) => {
   const [index, setIndex] = React.useState<number>(0);
-
   const onSwiped = (): void => {
     setIndex((index + 1) % jobs.length);
+  };
+
+  const saveOrRejectCurrentJob = (key: string, index: number) => {
+    AsyncStorage.getItem(key, (err, result) => {
+      let obj = {};
+      if (result !== null) {
+        console.log("Data Found");
+        obj = JSON.parse(result);
+      } else {
+        console.log("Data Not Found");
+      }
+      obj[jobs[index].id] = jobs[index];
+      AsyncStorage.setItem(key, JSON.stringify(obj));
+    });
   };
 
   return (
@@ -24,6 +37,12 @@ export const SwipeForJobs = ({ jobs }: SwipeForJobsProps) => {
         cardIndex={index}
         renderCard={(job) => <JobCard job={job} />}
         onSwiped={onSwiped}
+        onSwipedRight={() => saveOrRejectCurrentJob("savedJobs", index)}
+        onSwipedLeft={() => {
+          saveOrRejectCurrentJob("rejectedJobs", index);
+          // AsyncStorage.clear(); 
+          // use AsyncStorage.clear() to clear storage. 
+        }}
         stackSize={jobs.length} // how many items
         stackScale={10} // how much to shrink in percentage
         stackSeparation={14} // spacing between each item
